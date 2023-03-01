d2e0a79feaed832dcb4ae8628d3555c033a2062b
Noel <ndalfaro333@gmail.com>
2023-03-01 00:55:15 -0600

    adding upgrade functionality in buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,72 @@
 import './App.css';
-import SamosaImg from "./images/Punjabi-Samosa-4.jpg"; 
+import SamosaImg from "./images/samosa.png"
 import { useState } from 'react';
 const App = () => {
 
   const [count, setCount] = useState(0);
 
-  const [multliplier, setMultliplier] = useState(1); 
+  const [multiplier, setMultiplier] = useState(1); 
 
 
   const updateCount = () => {
-    setCount(count + multliplier); 
+    setCount(count + multiplier); 
   }
+
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+      setCount(count - 10);
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  }
+
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier( multiplier * 10);
+      setCount(count - 1000);
+    }
+  }
+
+
 
   return (
     <div className="App">
+
+      
       <header>
         <h1>Samosa Selector</h1>
         <h2>Count: {count}</h2>
         <img onClick={updateCount} src={SamosaImg}className='samosa' />
       </header>
+      
+      <div className='container'>
+        <div className='upgrade'>
+          <h3>Double Stuffed👯‍♀️</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
+        </div>
+        <div className='upgrade'>
+          <h3>PartyPack 🎉</h3>
+          <p>5x per click</p>
+          <button onClick={buyPartyPack}>100 samosas</button>
+
+
+        </div>
+        <div className='upgrade'>
+          <h3>Full Feast👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 samosas</button>
+
+        </div>
+
+      </div>
     </div>
   )
 }
